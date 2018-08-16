01294a03911981b01c2ec6c6bbbd10c4751109a2
Aaron Patterson <aaron.patterson@gmail.com>
2018-08-15 17:59:47 -0700

    Convert sample to tree
    Now we can walk the sample and display it

--- lib/allocation_sampler.rb
+++ lib/allocation_sampler.rb
@@ -3,41 +3,89 @@ module ObjectSpace
   class AllocationSampler
     VERSION = '1.0.0'
 
-    def display io = $stdout
-      if location?
-        display_with_location result, io
-      else
-        display_without_location result, io
+    module Nodes
+      class Node
+        attr_reader :children
+
+        def initialize children
+          @children = children
+          @count    = nil
+        end
+
+        def to_i
+          @count ||= children.map(&:to_i).inject :+
+        end
+
+        def pct of
+          to_i / of.to_i.to_f
+        end
       end
-    end
+      class Root < Node; end
 
-    private
+      class Type < Node
+        attr_reader :name
 
-    def display_with_location info, io
-      sum_allocations = Hash.new 0
-      info.each_pair { |class_name, locations|
-        sum_allocations[class_name] = locations.values.flat_map(&:values).inject(:+)
-      }
-      sample_count = sum_allocations.values.inject :+
+        def initialize name, children
+          @name = name
+          super(children)
+        end
+      end
 
-      records = info.sort_by { |name, _| sum_allocations[name] }.reverse
-      records.each do |name, paths|
-        io.puts "#{name} (#{pct(sum_allocations[name], sample_count)}):"
-        paths = paths.sort_by { |_, lines| lines.values.inject(:+) }.reverse
-        paths.each do |path, lines|
-          lines.sort_by { |_, count| count }.reverse.each do |line, count|
-            puts "  #{path}:#{line}: #{pct(count, sample_count)} #{pct(count, sum_allocations[name])}"
-          end
+      class Path < Type; end
+      class Line < Struct.new(:number, :to_i)
+        def pct of
+          to_i / of.to_i.to_f
         end
       end
     end
 
-    def pct a, b
-      "#{a} / #{b} (" + sprintf("%.2f", (a / b.to_f) * 100) + ")%"
+    module Aggregators
+      def self.location root, io = $stdout
+        Location.new.display root, io
+      end
+
+      class Location
+        def display root, io, min = 0.05
+          root.children.sort_by(&:to_i).reverse_each do |type|
+            next if pct(type, root) < min
+
+            puts "#{type.name} #{pct_s(type, root)}"
+            type.children.sort_by(&:to_i).reverse_each do |path|
+              next if pct(path, type) < min
+              puts "  #{path.name} #{pct_s(path, root)} #{pct_s(path, type)}"
+
+              path.children.sort_by(&:to_i).reverse_each do |line|
+                next if pct(line, path) < min && path.children.length > 1
+                puts "    #{line.number} #{pct_s(line, root)}"
+              end
+            end
+          end
+        end
+
+        def pct a, b
+          a.to_i / b.to_i.to_f
+        end
+
+        def pct_s a, b
+          "#{a.to_i} / #{b.to_i} (" + sprintf("%.2f", pct(a, b) * 100) + ")%"
+        end
+      end
     end
 
-    def display_without_location info
-      raise
+    def tree
+      if location?
+        Nodes::Root.new result.map { |type, files|
+          Nodes::Type.new type, files.map { |path, lines|
+            Nodes::Path.new path, lines.map { |line, count|
+              Nodes::Line.new line, count
+            }
+          }
+        }
+      else
+        Nodes::Root.new result.map { |type, count|
+          Nodes::Type.new type, count
+        }
+      end
     end
   end
 end
